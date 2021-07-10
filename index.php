ec273113b4f900e0806843c2bd83942a9c67989b
kryse <kanseidorifto77@gmail.com>
2021-07-10 20:47:41 +0800

    rev

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>SafeAlert</title>
+	<title>Not SafeAlert</title>
 	<?php
 		//include 'header.php';
 		include 'main.php';
 	?>
 </head>
 <body>
-
+	test safealert yo
 </body>
 </html>
