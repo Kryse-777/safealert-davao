38cf267231e13e492704db9f7fe362709b370d37
unknown <jpacquiao77@gmail.com>
2022-01-12 07:51:37 +0800

    marker test4 fix

--- marker.php
+++ marker.php
@@ -3,16 +3,18 @@ if (session_status()==PHP_SESSION_NONE)
 {
     session_start();
 }
-    var circle2 = L.circle([7.1524, 125.6588], {
+
+?>
+<html>
+<head>
+<script>
+    var circle1 = L.circle([7.1524, 125.6588], {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5,
         radius: 200
     }).addTo(map);
-?>
-<html>
-<head>
-
+</script>
 </head>
 <body>
 
