a61acdfa74299854d33eba4241d1c9b9087a1b53
unknown <jpacquiao77@gmail.com>
2022-01-12 14:10:06 +0800

    update

--- main.php
+++ main.php
@@ -171,7 +171,7 @@ if(!isset($_SESSION['username'])){
         },2500);
     }
     var markerme, circleme;
-    alert(trackme);
+    //alert(trackme);
     function getPosition(position){
         // console.log(position)
         var lat = position.coords.latitude
