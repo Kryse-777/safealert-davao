d02d63c0e8b9990142cc5b8b0fbd1125dd782909
unknown <jpacquiao77@gmail.com>
2022-01-12 11:20:32 +0800

    marker test6

--- marker.php
+++ marker.php
@@ -7,13 +7,43 @@ if (session_status()==PHP_SESSION_NONE)
 ?>
 <html>
 <head>
+
+//preinserted data
 <script>
+
+    //panacan
     var circle1 = L.circle([7.1524, 125.6588], {
         color: 'red',
         fillColor: '#ffaa00',
         fillOpacity: 0.5,
         radius: 200
     }).addTo(map);
+
+    //maa
+    var circle2 = L.circle([7.101813, 125.582199], {
+        color: 'red',
+        fillColor: '#ffaa00',
+        fillOpacity: 0.5,
+        radius: 200
+    }).addTo(map);
+
+    //cabantian
+    var circle3 = L.circle([7.1290, 125.6173], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 200
+    }).addTo(map);
+
+    //tibungco
+    var circle4 = L.circle([7.1959, 125.6416], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 200
+    }).addTo(map);
+
+
 </script>
 </head>
 <body>
