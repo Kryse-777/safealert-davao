f094b3770afe467a85d5b8505e7d48fc3b625b67
unknown <jpacquiao77@gmail.com>
2022-01-13 15:46:44 +0800

    update integ db fix15

--- loadmarker.php
+++ loadmarker.php
@@ -13,7 +13,7 @@
         fillOpacity: 0.5,
             radius: 1500
         }).addTo(safeadmap);
-        //circle". $row['id'] .".bindPopup('". $row['area'] ."<br/>Risk Assessment:" . $row['risk'] ." Risk');
+        circle". $row['id'] .".bindPopup('". $row['area'] ."<br/>Risk Assessment:" . $row['risk'] ." Risk');
     
         //create quadrants for circle
         var Quadrant1". $row['id'] ." = createQuadrant(circle". $row['id'] .",0).addTo(safeadmap);
