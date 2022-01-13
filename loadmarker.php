265fa64c19f64e6b6c303482786510ffd1dd7da4
unknown <jpacquiao77@gmail.com>
2022-01-13 15:48:05 +0800

    ui fix

--- loadmarker.php
+++ loadmarker.php
@@ -29,7 +29,7 @@
             if(inPolygon){
                 quadrant.setStyle({color: 'red'});
                 markerme.bindPopup('Focus Override<br/>Warning: You are on a COVID Risk Area, be wary of your surroundings'
-                    +'and vacate the premises as soon as possible')
+                    +' and vacate the premises as soon as possible')
                 markerme.openPopup()
                 //notifyMe();
                 //alert('Warning: You are on a COVID Risk Area, be wary of your surroundings and vacate the premises as soon as possible');
