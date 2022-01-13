a471c79b8701a1478a2ac3a65221c17252ca471b
unknown <jpacquiao77@gmail.com>
2022-01-13 16:35:21 +0800

    ui fix12

--- detectarea.php
+++ detectarea.php
@@ -1,19 +1,19 @@
 <?php
+    echo "<script>";
+    echo "setInterval(() => {
+                        navigator.geolocation.getCurrentPosition(getQuadrant)
+                    },time)
+                    function getQuadrant(){";
     $query = mysqli_query($safealertdb, "SELECT * FROM riskarea");
     while($row = mysqli_fetch_array($query))
     {
         //$row1=$row;
         //$row1--;
-        echo "<script>";
-        echo "setInterval(() => {
-                    navigator.geolocation.getCurrentPosition(getQuadrant)
-                },time)
-                function getQuadrant(){
-                    inQuadrant(Quadrant1" . $row['id'] . ",markerme)
-                    inQuadrant(Quadrant2" . $row['id'] . ",markerme)
-                    inQuadrant(Quadrant3" . $row['id'] . ",markerme)
-                    inQuadrant(Quadrant4" . $row['id'] . ",markerme)
-                }";
-        echo "</script>";
+        echo "inQuadrant(Quadrant1" . $row['id'] . ",markerme)
+            inQuadrant(Quadrant2" . $row['id'] . ",markerme)
+            inQuadrant(Quadrant3" . $row['id'] . ",markerme)
+            inQuadrant(Quadrant4" . $row['id'] . ",markerme)";
     }
+    echo"}";
+    echo "</script>";
 ?>
