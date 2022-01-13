7815873959cdb0036f6cd781992af34990850976
unknown <jpacquiao77@gmail.com>
2022-01-13 15:34:41 +0800

    update integ db fix12

--- main.php
+++ main.php
@@ -233,20 +233,20 @@ if(!isset($_SESSION['username'])){
     //include 'davaomap.php';
     $query = mysqli_query($safealertdb, "SELECT * FROM riskarea");
     while($row = mysqli_fetch_array($query))
-    $row1=$row;
-    $row1--;
+    //$row1=$row;
+    //$row1--;
     echo "<script>";
     echo "setInterval(() => {
                 navigator.geolocation.getCurrentPosition(getQuadrant)
             },125)
             function getQuadrant(){
-                inQuadrant(Quadrant1". $row1['id'] .",markerme)
-                inQuadrant(Quadrant2". $row1['id'] .",markerme)
-                inQuadrant(Quadrant3". $row1['id'] .",markerme)
-                inQuadrant(Quadrant4". $row1['id'] .",markerme)
+                inQuadrant(Quadrant1". $row['id'] .",markerme)
+                inQuadrant(Quadrant2". $row['id'] .",markerme)
+                inQuadrant(Quadrant3". $row['id'] .",markerme)
+                inQuadrant(Quadrant4". $row['id'] .",markerme)
             }";
     echo "</script>";
-    $row++;
+    //$row++;
 
 ?>
 </html>
