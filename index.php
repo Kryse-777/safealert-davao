fa8fc7cb1bed53e05564769e40444b55389a2cd4
unknown <jpacquiao77@gmail.com>
2022-04-14 08:06:18 +0800

    ui update and fixes

--- index.php
+++ index.php
@@ -120,7 +120,7 @@ if(!isset($_SESSION['username'])){
         <div class="tab-pane fade" id="info">
             <h4 class="acch mt-2">Essential COVID-19 Information</h4>
             <div class="covupdate">
-                <span>COVID-19 Information in Davao City as of January 9-16, 2022</span>
+                <span>COVID-19 Information in Davao City as of April 10-16, 2022</span>
             </div>
             <!-- Info Table -->
             <table id="risktable" class="infotable sortable table table-striped">
@@ -173,7 +173,7 @@ if(!isset($_SESSION['username'])){
             <div id="locstat">
                 <b>
                     Davao City Overall Risk Classification:
-                    <?php>
+                    <?php
                         $result = mysqli_query($safealertdb, "SELECT * FROM avgrisk");
                         while($row = mysqli_fetch_array($result)){
                             if($row['riskass']=='Minimal') {
@@ -181,7 +181,6 @@ if(!isset($_SESSION['username'])){
                             }
                             echo " " . $row['riskass'] . "<br/>";
                         }
-
                     ?>
                     Alert Level: 3<br/><br/>
 
